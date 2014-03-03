7e163e961fa827348c2c82eb9f9ed546bc8e07da
jeanfabre <contact@fabrejean.net>
2014-03-03 19:34:19 +0400

    more dépendances

--- Assets/PlayMaker Custom Actions/GUI/GUIBeginSize.cs
+++ Assets/PlayMaker Custom Actions/GUI/GUIBeginSize.cs
@@ -1,10 +1,12 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
 // PLAYMAKER ECOSYSTEM DO NOT EDIT
 /*---
+EcoMetaStart
 {
 "type":"__ECO_ACTION__",
 "script dependancies":["GUISizer.cs"]
 }
+EcoMetaEnd
 ---*/
 
 using UnityEngine;
