032488acc6f40cb072f1431760fd80c3a9e0ae74
jeanfabre <contact@fabrejean.net>
2014-03-03 19:12:34 +0400

    modified meta for easy regel

--- Assets/PlayMaker Custom Actions/API/FsmVector4Action.cs
+++ Assets/PlayMaker Custom Actions/API/FsmVector4Action.cs
@@ -1,10 +1,11 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
-/* PLAYMAKER ECOSYSTEM DO NOT EDIT
+// PLAYMAKER ECOSYSTEM DO NOT EDIT
+/*--- 
 {
 "type":"__ECO_ACTION__",
 "script dependancies":["FsmVector4ActionEditor.cs"]
 }
-*/
+---*/
 
 using UnityEngine;
 
--- Assets/PlayMaker Custom Actions/GUI/GUIBeginSize.cs
+++ Assets/PlayMaker Custom Actions/GUI/GUIBeginSize.cs
@@ -1,10 +1,12 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
-/* PLAYMAKER ECOSYSTEM DO NOT EDIT
+// PLAYMAKER ECOSYSTEM DO NOT EDIT
+/*---
 {
 "type":"__ECO_ACTION__",
 "script dependancies":["GUISizer.cs"]
 }
-*/
+---*/
+
 using UnityEngine;
 using System.Collections.Generic;
 
