8af892c12f45a2df6061d66b07651efb6fd764ab
jeanfabre <contact@fabrejean.net>
2014-03-25 09:17:20 +0400

    cleaned up meta data

--- Assets/PlayMaker Custom Actions/GameObject/FindClosest2.cs
+++ Assets/PlayMaker Custom Actions/GameObject/FindClosest2.cs
@@ -1,13 +1,9 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
 // Based on FindClosest
 // Added Ignore Owner option. Thanks Nueral Echo: http://hutonggames.com/playmakerforum/index.php?topic=71.0
-/*--- __ECO__ __ACTION__
-EcoMetaStart
-{
-"type":"ACTION"
-}
-EcoMetaEnd
----*/
+
+/*--- __ECO__ __ACTION__ ---*/
+
 using UnityEngine;
 
 namespace HutongGames.PlayMaker.Actions
