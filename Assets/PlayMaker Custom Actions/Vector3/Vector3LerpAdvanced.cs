de5ebedaf60e8575d786a48e34f1aba6a673d230
jeanfabre <contact@fabrejean.net>
2014-07-14 15:57:07 +0400

    Fixed dependances meta information for Vector3LerpAdvanced

--- Assets/PlayMaker Custom Actions/Vector3/Vector3LerpAdvanced.cs
+++ Assets/PlayMaker Custom Actions/Vector3/Vector3LerpAdvanced.cs
@@ -1,5 +1,13 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
-/*--- __ECO__ __ACTION__ ---*/
+/*--- __ECO__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+						"Assets/PlayMaker Custom Actions/__Internal/PlayMakerActionsUtils.cs"
+					]
+}
+EcoMetaEnd
+---*/
 
 using UnityEngine;
 
