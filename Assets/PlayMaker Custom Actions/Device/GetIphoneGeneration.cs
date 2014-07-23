189c8d6372cec0907b7456a7c553a27c97b97406
jeanfabre <contact@fabrejean.net>
2014-07-23 16:43:47 +0400

    New actions

---
+++ Assets/PlayMaker Custom Actions/Device/GetIphoneGeneration.cs
@@ -0,0 +1,35 @@
+// (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
+/*--- __ECO__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Device)]
+	[Tooltip("Gets the iPhone generation.")]
+	public class GetIphoneGeneration : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The iPhone generation.")]
+		public FsmString iphoneGeneration;
+		
+		
+		public override void Reset()
+		{
+			iphoneGeneration = null;
+		}
+		
+		public override void OnEnter()
+		{
+			DoGetIphoneGeneration();
+			Finish();
+		}
+		
+		void DoGetIphoneGeneration()
+		{
+			iphoneGeneration.Value = iPhone.generation.ToString();
+		}
+		
+	}
+}
