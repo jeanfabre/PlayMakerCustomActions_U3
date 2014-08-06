095c24478f69e2dcc79ae395a52833d45d5fa4d9
jeanfabre <contact@fabrejean.net>
2014-08-06 14:31:07 +0400

    Fixed device orientation since we cna't have everyframe option...

--- Assets/PlayMaker Custom Actions/Device/SetDeviceScreenOrientation.cs
+++ Assets/PlayMaker Custom Actions/Device/SetDeviceScreenOrientation.cs
@@ -6,29 +6,21 @@ using UnityEngine;
 namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory(ActionCategory.Device)]
-	[Tooltip("Specifies logical orientation of the screen. Default value is taken from the 'Default Orientation' in Player Settings. Currently screen orientation is only relevant on mobile platforms")]
+	[Tooltip("Specifies logical orientation of the screen. Currently screen orientation is only relevant on mobile platforms")]
 	public class SetDeviceScreenOrientation : FsmStateAction
 	{
 		[Tooltip("Amount of acceleration under which to trigger the event. Use low numbers.")]
 		public ScreenOrientation orientation;
 		
-		[Tooltip("Repeat every frame. Useful if any of the values are changing.")]
-		public bool everyFrame;
-		
 		public override void Reset()
 		{
 			orientation = ScreenOrientation.Landscape;
-			everyFrame = false;
 		}
 		
-		public override void OnUpdate()
+		public override void OnEnter()
 		{
 			Screen.orientation = orientation;
-			
-			if (!everyFrame)
-			{
-				Finish();
-			}
+			Finish();
 		}
 
 	}
