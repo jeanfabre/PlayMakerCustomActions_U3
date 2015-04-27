3f65ba56562205fd20b154e0990c2b73dcddbbac
jeanfabre <contact@fabrejean.net>
2015-04-27 14:09:54 +0300

    new action to deal with singletons

---
+++ Assets/PlayMaker Custom Actions/GameObject/SingletonManager.cs
@@ -0,0 +1,39 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+/* __ECO__ __ACTION__ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HutongGames.PlayMaker.Actions{
+	
+	[ActionCategory(ActionCategory.Logic)]
+	[Tooltip("Manager Singletons. If the reference given exists already, then this gameobject is destroyed")]
+	public sealed class SingletonManager : FsmStateAction{
+
+		public static List<string> SINGLETONS;
+
+		public FsmString reference;
+
+		public override void Reset(){
+			reference = "My Singleton";
+		}
+		
+		public override void OnEnter(){
+
+			if (SINGLETONS==null)
+			{
+				SINGLETONS = new List<string>();
+				SINGLETONS.Add(reference.Value);
+			}else{
+				if (SINGLETONS.Contains(reference.Value))
+				{
+					Object.Destroy(Owner);
+					return;
+				}
+			}
+
+			Finish();		
+		}
+	}
+}
