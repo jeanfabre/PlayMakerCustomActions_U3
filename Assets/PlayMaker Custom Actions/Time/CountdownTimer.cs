449bfd158117fb248976489f6f3a3382269bf504
djaydino <djay_dino@hotmail.com>
2015-09-26 12:25:02 +0200

    Update CountdownTimer.cs

--- Assets/PlayMaker Custom Actions/Time/CountdownTimer.cs
+++ Assets/PlayMaker Custom Actions/Time/CountdownTimer.cs
@@ -1,6 +1,6 @@
 // (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
 // Action made by DjayDino
-/*--- __ECO__ __ACTION__ ---*/
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 
 using UnityEngine;
 
