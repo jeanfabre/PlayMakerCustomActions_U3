f2bfe24d379049931a5ce28c21fb0cc5cb84ffe6
djaydino <djay_dino@hotmail.com>
2015-09-26 12:02:35 +0200

    Update MultiplyVelocity.cs

--- Assets/PlayMaker Custom Actions/Physics/MultiplyVelocity.cs
+++ Assets/PlayMaker Custom Actions/Physics/MultiplyVelocity.cs
@@ -1,5 +1,5 @@
 // (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
-/*--- __ECO__ __ACTION__---*/
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 /* original action by trentSterling on http://hutonggames.com/playmakerforum/index.php?topic=8416.msg32820 */
 
 using UnityEngine;
