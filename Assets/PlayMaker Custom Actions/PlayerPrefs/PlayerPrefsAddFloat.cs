c1817efbc861fc7e293caf5d871516891ebf984b
djaydino <djay_dino@hotmail.com>
2016-03-26 13:13:21 +0100

    Update PlayerPrefsAddFloat.cs
    Action was broken (null refference)
    updated and should work on all versions

--- Assets/PlayMaker Custom Actions/PlayerPrefs/PlayerPrefsAddFloat.cs
+++ Assets/PlayMaker Custom Actions/PlayerPrefs/PlayerPrefsAddFloat.cs
@@ -1,6 +1,6 @@
-// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
 // Action made by DjayDino
-/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+/*--- __ECO__ __ACTION__ ---*/
 // @keyword: playerpref player pref preference
 
 using UnityEngine;
@@ -13,23 +13,22 @@ namespace HutongGames.PlayMaker.Actions
 		[Tooltip("Case sensitive key.")]
 		public FsmString key;
 		public FsmFloat add;
-		private FsmFloat variables;
+		private float variables;
 
 		public override void Reset()
 		{
 			key = "";
-			variables = new FsmFloat();
 			add = null;
 		}
 
 		public override void OnEnter()
 		{
 				if(!key.IsNone || !key.Value.Equals(""))  
-				variables.Value = PlayerPrefs.GetFloat(key.Value, variables.IsNone ? 0f : variables.Value);
+				variables = PlayerPrefs.GetFloat(key.Value, 0f);
 				
-				variables.Value += add.Value;
+				variables += add.Value;
 				
-				PlayerPrefs.SetFloat(key.Value, variables.IsNone ? 0f : variables.Value);
+				PlayerPrefs.SetFloat(key.Value, (variables));
 
 			Finish();
 		}
