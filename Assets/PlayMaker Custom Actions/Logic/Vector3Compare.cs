d96e867009cc957efb4bfb928539cb482340c2f9
Jean Fabre <contact@fabrejean.net>
2016-05-25 11:35:20 +0300

    added fsmbool to save comparison on Vector3Compare action

--- Assets/PlayMaker Custom Actions/Logic/Vector3Compare.cs
+++ Assets/PlayMaker Custom Actions/Logic/Vector3Compare.cs
@@ -15,6 +15,10 @@ namespace HutongGames.PlayMaker.Actions
 		public FsmVector3 vector3Variable2;
 		[RequiredField]
 		public FsmFloat tolerance;
+
+		[Tooltip("true Vector3 1 equals Vector3 2 (within Tolerance)")]
+		public FsmBool result;
+
 		[Tooltip("Event sent if Vector3 1 equals Vector3 2 (within Tolerance)")]
 		public FsmEvent equal;
 		[Tooltip("Event sent if the Vector3's are not equal")]
@@ -26,6 +30,7 @@ namespace HutongGames.PlayMaker.Actions
 			vector3Variable1 = null;
 			vector3Variable2 = null;
 			tolerance = 0f;
+			result = null;
 			equal = null;
 			notEqual = null;
 			everyFrame = false;
@@ -52,20 +57,14 @@ namespace HutongGames.PlayMaker.Actions
 			    && Mathf.Abs(vector3Variable1.Value.y - vector3Variable2.Value.y) <= tolerance.Value
 			    && Mathf.Abs(vector3Variable1.Value.z - vector3Variable2.Value.z) <= tolerance.Value)
 			{
+				result.Value = true;
 				Fsm.Event(equal);
-				return;
+
 			} else {
+				result.Value = false;
 				Fsm.Event(notEqual);
 			}
 			
-		}
-		
-		public override string ErrorCheck()
-		{
-			if (FsmEvent.IsNullOrEmpty(equal) &&
-			    FsmEvent.IsNullOrEmpty(notEqual))
-				return "Action sends no events!";
-			return "";
 		}
 	}
 }
